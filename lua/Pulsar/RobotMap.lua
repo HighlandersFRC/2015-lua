8652ef6ca503b6e761c80b35197a57aa903a6196
John <jwiens3141@gmail.com>
2015-01-18 13:40:34 -0700

    This is the lifter script that should allow us to move the platform up and down as well as in and out.

--- lua/Pulsar/RobotMap.lua
+++ lua/Pulsar/RobotMap.lua
@@ -5,7 +5,9 @@ BLTalon = WPILib.CANTalon(2),
 FRTalon = WPILib.CANTalon(3),
 BRTalon = WPILib.CANTalon(4),
 rightIntake = WPILib.CANTalon(5),
-leftIntake = WPILib.CANTalon(6)
+leftIntake = WPILib.CANTalon(6),
+lifterUpDown = WPILib.CANTalon(7),
+lifterInOUt = WPILib.CANTalon(8)
 }
 
 -- talon five right intake 
---
+++ lua/Pulsar/lifter.lua
@@ -0,0 +1,93 @@
+--Intake Pulsar
+
+--debugPrint("intake started")
+local core = require"core"
+local lifterPower = .5
+local lifterInOutPower = .5
+
+
+-- Intake In Command
+local lifterOut = {
+  Initialize = function()
+    -- motor code goes here
+    robotMap.lifterInOut:Set(-lifterInOutPower)
+  end,
+  IsFinished = function() 
+    return not OI.lifterOut:Get()  
+  end,
+  End = function(self)
+    robotMap.lifterInOut:Set(0)
+  end,
+  Interrupted = function(self)
+    self:End()
+  end,
+  subsystems = {
+    "LifterInOut"
+  },
+}
+
+local lifterIn = {
+  Initialize = function()
+    -- motor code goes here
+    robotMap.lifterUpDown:Set(lifterInOutPower)
+  end,
+  IsFinished = function() 
+    return not OI.lifterIn:Get()  
+  end,
+  End = function(self)
+    robotMap.lifterInOut:Set(0)
+  end,
+  Interrupted = function(self)
+    self:End()
+  end,
+  subsystems = {
+    "LifterInOut"
+  },
+}
+
+local lifterUp = {
+  Initialize = function()
+    -- motor code goes here
+    robotMap.lifterUpDown:Set(-lifterPower)
+  end,
+  IsFinished = function() 
+    return not OI.lifterUp:Get()  
+  end,
+  End = function(self)
+    robotMap.lifterUpDown:Set(0)
+  end,
+  Interrupted = function(self)
+    self:End()
+  end,
+  subsystems = {
+    "LifterUpDown"
+  },
+}
+
+local lifterDown = {
+  Initialize = function()
+    -- motor code goes here
+    robotMap.lifterUpDown:Set(lifterPower)
+  end,
+  IsFinished = function() 
+    return not OI.lifterDown:Get()  
+  end,
+  End = function(self)
+    robotMap.lifterUpDown:Set(0)
+  end,
+  Interrupted = function(self)
+    self:End()
+  end,
+  subsystems = {
+    "LifterUpDown"
+  },
+}
+-- triggers
+
+Robot.scheduler:AddTrigger(triggers.whenPressed(OI.lifterDown,lifterDown))
+Robot.scheduler:AddTrigger(triggers.whenPressed(OI.lifterUp,lifterUp))
+
+--Robot.scheduler:SetDefaultCommand("intakeWheels",intakeWheelsStopCommand)
+--Robot.Teleop.Put("intakeWheels", intakeWheelsStopCommand)
+debugPrint("Lifter Finished")
+
