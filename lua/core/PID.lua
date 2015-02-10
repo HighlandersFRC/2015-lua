c7d003c29a87f66d5b58fdb77ee6374e1926062f
John <jwiens3141@gmail.com>
2015-02-09 20:08:10 -0700

    Updated PID controller

--- lua/core/PID.lua
+++ lua/core/PID.lua
@@ -1,14 +1,20 @@
 
 local function PIDupdate(pid, val)
   local outval = 0
+  local deltaTime = WPILib.Timer.GetFPGATimestamp() - pid.lastTime
   outval = outval + pid.kP * (pid.setpoint - val)
-  pid.accum = pid.accum + (pid.setpoint - val)
+  pid.accum = pid.accum + (pid.setpoint - val) * deltaTime
   outval = outval + pid.kI * pid.accum
-  outval = outval + pid.kD * (pid.last - val)
-  pid.last = val
+  outval = outval + pid.kD/deltaTime * (pid.last - val) 
+  pid.last = val 
+  pid.lastTime = WPILib.Timer.GetFPGATimestamp()
   return outval
 end
-
+local PIDenable = function()
+  
+  
+  PID.lastTime = WPILib.Timer.GetFPGATimestamp()
+  end
 
 local createPID = function(KP,KI,KD) 
   PID = {
@@ -18,7 +24,9 @@ local createPID = function(KP,KI,KD)
   kP = KP,
   kI = KI,
   kD = KD,
-  Update = PIDupdate
+  lastTime = WPILib.Timer.GetFPGATimestamp(),
+  Update = PIDupdate,
+  enable = PIDenable
 }
 return PID
 end
