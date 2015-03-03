3762091940eeee46815e222aeca4be55ac911702
Alex Iverson <alexjiverson@gmail.com>
2015-03-03 14:21:45 -0700

    Created commands to print and publish values.

---
+++ lua/command/Print.lua
@@ -0,0 +1,23 @@
+return function(...)
+  local printCommand = {vals = {...}}
+
+  function printCommand:Initialize()
+    print(table.unpack(self.vals))
+  end
+  function printCommand:Execute()
+    --do nothing
+  end
+  function printCommand:End()
+    --do nothing
+  end
+  function printCommand:Interrupted()
+    --do nothing
+  end
+  function printCommand:IsFinished()
+    return true
+  end
+  function printCommand:IsInterruptible()
+    return true
+  end
+  return printCommand
+end
---
+++ lua/command/Publish.lua
@@ -0,0 +1,23 @@
+return function(topic, message)
+  local publishCommand = {topic = topic, message = message}
+
+  function publishCommand:Initialize()
+    publish(self.topic, self.message)
+  end
+  function publishCommand:Execute()
+    --do nothing
+  end
+  function publishCommand:End()
+    --do nothing
+  end
+  function publishCommand:Interrupted()
+    --do nothing
+  end
+  function publishCommand:IsFinished()
+    return true
+  end
+  function publishCommand:IsInterruptible()
+    return true
+  end
+  return publishCommand
+end
