c931bb7fbee4fef51579fac2c1807984e7c0223a
John <jwiens3141@gmail.com>
2015-03-07 13:05:21 -0700

    One Tote supposibly working

--- lua/Pulsar/Auto/OneTote.lua
+++ lua/Pulsar/Auto/OneTote.lua
@@ -20,25 +20,25 @@ print("oneTote 2")
 
 local fullAutonomous = sequence(
   
-  liftMacro(22),
+  start(liftMacro(22,00)),
+  wait(.25),
   start(intake(-1, 1.5)),
   drive(0.2, 2),
   inOut(RobotConfig.lifterInOutMin, 1),
-  liftMacro(0),
-  wait(0.25),
-  liftMacro(22),
- -- start(holding(22)),
+  --liftMacro(0,100),
+  wait(0.5),
+  --liftMacro(22,200),
   wait(.2),
   spin(110),
   wait(.2),
-  drive(0.3, 3),
+  drive(0.3, 3.6),
   spin(180),
-  inOut(50, 1.5),
+  --inOut(50, 1.5),
   start(intake(1, 1.2)),
   wait(.5),
-  liftMacro(0),
+  --liftMacro(0,100),
   wait(.5),
-  inOut(1, 1),
+  --inOut(1, 1),
   drive(-0.3, 0.45)
   
 )
