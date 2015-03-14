fd5112cabb8fbb9e585d58767a839152781ba67b
Tyler <mysticalpancake@gmail.com>
2015-03-13 20:53:41 -0600

    Implemented print max/min values from ArduLidar.

--- lua/sensorTests/ArduLidar.lua
+++ lua/sensorTests/ArduLidar.lua
@@ -2,11 +2,13 @@ local core = require"core"
 core.setBasicRobot()
 local arduino = io.open("/dev/ttyACM0")
 local prevTime = 0
+local maxMinTime = 0
 local lastFragment = ""
-
+local min = nil
 Robot.Disabled = {
   Execute = function()
-    if prevTime + 0.05 < WPILib.Timer.GetFPGATimestamp() then
+    local currentTime = WPILib.Timer.GetFPGATimestamp()
+    if (prevTime + .2) < currentTime then
       local recv = arduino:read(32)
       local offset = recv:find("\n")
       local sensorVal = tonumber(lastFragment..recv:sub(1, offset))
@@ -19,7 +21,23 @@ Robot.Disabled = {
         offset = recv:find("\n", offset+1)
       end
       lastFragment = recv:sub(prevOffset+1, -1)
+      if min == nil then
+        min = tonumber(sensorVal)
+        max = tonumber(sensorVal)
+      end
       print(sensorVal)
+      prevTime = WPILib.Timer.GetFPGATimestamp()
+      if tonumber(sensorVal) > max then
+        max = tonumber(sensorVal)
+      end
+      if tonumber(sensorVal) < min then
+        min = tonumber(sensorVal)
+      end
+      if (maxMinTime + 5) < currentTime then
+        print("min >>>", min,"              max >>>", max)
+        maxMinTime = WPILib.Timer.GetFPGATimestamp()
+       min = nil
+      end
     end
   end
 }
