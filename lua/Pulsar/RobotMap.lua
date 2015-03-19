183be9e23b8276455e3ed4aea5243971334530ca
Alex Iverson <alexjiverson@gmail.com>
2015-03-19 13:27:46 -0600

    Added persistent reference to a talon hopefully fixing a problem with garbage collection.

--- lua/Pulsar/RobotMap.lua
+++ lua/Pulsar/RobotMap.lua
@@ -16,12 +16,12 @@ FLTalon = core.getCanTalon(1),
   leftIntake = core.getCanTalon(6),
   lifterUpDown = core.getCanTalon(7),
   lifterInOut = core.getCanTalon(9),
-  navXport = port,
+  --navXport = port,
   navX = WPILib.AHRS(port,50),
   tail = core.getCanTalon(10)
 }
 local lifterUpDownTwo = core.getCanTalon(8)
 lifterUpDownTwo:SetControlMode(WPILib.CANTalon.kFollower)
 lifterUpDownTwo:Set(7)
-
+robotMap.lifterUpDownTwo = lifterUpDownTwo
 
