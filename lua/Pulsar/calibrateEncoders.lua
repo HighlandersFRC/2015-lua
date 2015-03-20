4f0de604f2a3edef227c8c49ca15ae29b638e6f7
Alex Iverson <alexjiverson@gmail.com>
2015-03-20 11:00:31 -0600

    fixed LifterUpDown calibration sequence.

--- lua/Pulsar/calibrateEncoders.lua
+++ lua/Pulsar/calibrateEncoders.lua
@@ -2,19 +2,28 @@ local corocmd = require"command.CoroutineCommand"
 print("Calibration Started")
 local calibration = function()
   local calibrating = function()
-    while( not robotMap.lifterUpDown:IsFwdLimitSwitchClosed()) do 
+    local count = 0
+    while( not robotMap.lifterUpDown:IsRevLimitSwitchClosed()) do 
       robotMap.lifterUpDown:Set(-.1)
+      if count % 50 == 0 then
+        print("waiting for limit switch", robotMap.lifterUpDown:IsFwdLimitSwitchClosed(), robotMap.lifterUpDown:IsRevLimitSwitchClosed())
+      end
+      count = count + 1
       coroutine.yield()
     end
 
     robotMap.lifterUpDown:Set(0)
     local position = -1*robotMap.lifterUpDown:GetPosition()
-    while math.abs(-1*robotMap.lifterUpDown:GetPosition() - position) < 500 do 
-      robotMap.lifterUpDown:Set(.1)
+    while math.abs(-1*robotMap.lifterUpDown:GetPosition() - position) < 500 do
+      if count % 50 == 0 then
+        print("waiting for encoder", math.abs(-1*robotMap.lifterUpDown:GetPosition() - position))
+      end
+      count = count + 1
+      robotMap.lifterUpDown:Set(.2)
       coroutine.yield()
 
     end
-    while( not robotMap.lifterUpDown:IsFwdLimitSwitchClosed()) do 
+    while( not robotMap.lifterUpDown:IsRevLimitSwitchClosed()) do 
       robotMap.lifterUpDown:Set(-.05)
       coroutine.yield()
     end
