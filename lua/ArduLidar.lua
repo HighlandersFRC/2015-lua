ad9dcfcb1fa02c4a6d8a1117ec1e3dc079419df6
Alex Iverson <alexjiverson@gmail.com>
2015-03-21 16:16:58 -0600

    Added code to account for the zero value of a failed read from the lidar.

--- lua/ArduLidar.lua
+++ lua/ArduLidar.lua
@@ -21,6 +21,9 @@ local laneGen = lanes.gen("io", "string",  function()
       local newVal = tonumber(lastFragment..recv:sub(1, offset))
       -- print(2)
       if newVal then
+        if newVal == 0 then
+          newVal = 4096
+        end
         updateEMA(newVal)
       end
       -- print(3)
@@ -32,6 +35,9 @@ local laneGen = lanes.gen("io", "string",  function()
         --print("trying substr", recv:sub(prevOffset+1, offset), "giving", tonumber(recv:sub(prevOffset+1, offset)))
         newVal = tonumber(recv:sub(prevOffset+1, offset))
         if newVal then
+          if newVal == 0 then
+            newVal = 4096
+          end
           updateEMA(newVal)
         end
         prevOffset = offset
