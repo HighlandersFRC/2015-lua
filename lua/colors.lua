218658ff68e4d686e366ca84adb94d6fd792a24c
David Rohrbaugh <rohrbaugh.david17@libertycommon.org>
2015-04-01 16:54:50 -0600

    Edit comment for colorMatrix function

--- lua/colors.lua
+++ lua/colors.lua
@@ -189,7 +189,7 @@ c = {
 --  {{"color","color","color","color"},
 --   {"color","color","color","color"}}
 --    and prints it out in the same orientation replacing the strings with a band of color that is 2 characters wide
---See examples/ColoradoFlag.lua and examples/dealWithItGlasses for a better explanation.
+--See examples/ColoradoFlag.lua and examples/dealWithItGlasses.lua for a better explanation.
   colorMatrix = function(t)
     if type(t) == "table" then
       local s = ""
