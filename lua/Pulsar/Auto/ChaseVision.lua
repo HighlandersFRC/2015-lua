b0ad053592cdb1578b15171e07caa4a884c7cce3
John <jwiens3141@gmail.com>
2015-04-17 19:01:20 -0600

    Vision code that goes forward, finds, and intakes a tote

--- lua/Pulsar/Auto/ChaseVision.lua
+++ lua/Pulsar/Auto/ChaseVision.lua
@@ -4,10 +4,10 @@ local ang
 local inRange
 local x
 local y
-
+local lidar = require"ArduLidar"
 local alpha = 0.25
 local emaVal = 0
-
+local finished = false
 local function updateEMA(val)
   emaVal = alpha * val + (1-alpha)*emaVal
   return emaVal
@@ -15,7 +15,7 @@ end
 
 subscribe("Vision/Center", function(topic, payload)
     --local x, y = payload
-    print("Tote is at " .. payload)
+    --- print("Tote is at " .. payload)
     local index = string.find(payload, ", ")
     x = tonumber(string.sub(payload,1,index-1))
     y = tonumber(string.sub(payload,index+2))
@@ -31,9 +31,9 @@ subscribe("Vision/Center", function(topic, payload)
     end
 
 
-    print("angOffset: " .. angOffset)
+    --- print("angOffset: " .. angOffset)
     ang = updateEMA(robotMap.navX:GetYaw() + angOffset)
-    print("ang: " .. ang)
+    ---print("ang: " .. ang)
   end
 )
 
@@ -46,13 +46,14 @@ local spinTurn = function()
 
 -----------------------------
   local pidLoop = require"core.PID"
-  local PID = pidLoop(0.03,0,0.5)
+  local PID = pidLoop(0.03,0,0.02)
 -----------------------------
   local heading = 0
   local startTime = 0
   local lastTime = 0
   local turn = {
     Initialize = function()
+      finished = false
       PID.minOutput = -1
       PID.maxOutput = 1
       PID.minInput = -180
@@ -62,7 +63,9 @@ local spinTurn = function()
 
       print("SpinTurn turning")
       heading = 0
+
       PID.setpoint = ang or robotMap.navX:GetYaw()
+
       print"starting timestamps"
       startTime =  WPILib.Timer.GetFPGATimestamp()
       lastTime = WPILib.Timer.GetFPGATimestamp()
@@ -77,23 +80,45 @@ local spinTurn = function()
       elseif ang ~= nil then
         angVal = ang
       end
-      print("Setting pid target to " .. angVal .. "or " .. robotMap.navX:GetYaw())
+      -- print("Setting pid target to " .. angVal .. "or " .. robotMap.navX:GetYaw())
       PID.setpoint = ang or robotMap.navX:GetYaw()
+      if PID.setpoint == robotMap.navX:GetYaw() then
+        print("(chase vision) the yaw has not changed")
+
+      end
       heading = robotMap.navX:GetYaw()
       --print("Heading",heading)
       publish("Robot/Heading", heading)
       local response = PID:Update(heading)    
       lastTime = WPILib.Timer.GetFPGATimestamp()
 
+      print("(ChaseVision)Lidar values",lidar:Get())
 
-      if inRange then
-        Robot.drive:MecanumDrive_Cartesian(0,response,-.3)
+
+      if(lidar:Get() > 120) then
+        robotMap.leftIntake:Set(0)
+        robotMap.rightIntake:Set(0)
+        if inRange  then
+          Robot.drive:MecanumDrive_Cartesian(0,response,-.6)
+        else
+          Robot.drive:MecanumDrive_Cartesian(0, response, 0)
+        end
+      elseif lidar:Get() > 80 then
+        robotMap.leftIntake:Set(-1)
+        robotMap.rightIntake:Set(1)
+        Robot.drive:MecanumDrive_Cartesian(0, 0, -0.3)
+      elseif lidar:Get() > 40 then
+        robotMap.leftIntake:Set(-.5)
+        robotMap.rightIntake:Set(.5)
+        Robot.drive:MecanumDrive_Cartesian(0, 0, 0)
       else
-        Robot.drive:MecanumDrive_Cartesian(0, response, 0)
+        robotMap.leftIntake:Set(0)
+        robotMap.rightIntake:Set(0)
+        finished = true
       end
     end,
     IsFinished = function() 
-      return false
+      return finished
     end,
     End = function(self)
 
@@ -104,7 +129,7 @@ local spinTurn = function()
       self:End()
     end,
     subsystems = {
-      "DriveTrain"
+      "DriveTrain","intake"
     }
   }
   return turn
